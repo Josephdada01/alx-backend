e037495802794e492dc3cfc1fe472657fb8e45c2
Josephdada01 <ridwandada500@gmail.com>
2024-05-08 12:19:42 +0100

    implementing First in First out caching

---
+++ 0x01-caching/1-fifo_cache.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python3
+"""
+FIFO caching module
+"""
+BaseCaching = __import__('base_caching').BaseCaching
+
+
+class FIFOCache(BaseCaching):
+    """
+    FIFOCache that inherits from BaseCaching and is a caching system
+    """
+    def __init__(self):
+        """
+        overloading and to calling the parent init
+        """
+        super().__init__()
+
+    def put(self, key, item):
+        """
+        Method that assign to the dictionary self.cache_data the
+        item value for the key keyIf key or item is None,
+        this method will not do anything.
+        If the number of items in self.cache_data is higher that MAX_ITEMS:
+        you must discard the first item put in cache (FIFO algorithm)
+        you must print DISCARD: with the key discarded and following by newline
+        """
+        if key is None or item is None:
+            return
+        if len(self.cache_data) >= self.MAX_ITEMS:
+            first_item_key = next(iter(self.cache_data))
+            del self.cache_data[first_item_key]
+            print(f"DISCARD: {first_item_key}\n")
+        self.cache_data[key] = item
+
+    def get(self, key):
+        """
+        return the value in self.cache_data linked to key
+        """
+        if key is None or key not in self.cache_data:
+            return None
+        return self.cache_data[key]
