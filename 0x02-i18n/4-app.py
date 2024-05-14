6c7ee0abc225f723c6fb9db5c1b2ebf6f7eaacda
Josephdada01 <ridwandada500@gmail.com>
2024-05-14 17:16:12 +0100

    forcing the locale making sure each language link works

--- 0x02-i18n/4-app.py
+++ 0x02-i18n/4-app.py
@@ -27,7 +27,6 @@ def get_locale() -> str:
     if locale in app.config['LANGUAGES']:
         print(locale)
         return locale
-    
     return request.accept_languages.best_match(app.config['LANGUAGES'])
 
 
